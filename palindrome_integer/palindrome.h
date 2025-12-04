23f93e7663d343e91b57bfc8b080db3d33e3ed2c
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2025-12-04 10:41:19 +0100

    algo

---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,8 @@
+#ifndef _PALINDROME_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_palindrome(unsigned long n);
+
+#endif /* _PALINDROME_ */
---
+++ primegame/0-prime_game.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module 0
+"""
+
+def isWinner(x, nums):
+    """Determine the winner based on the number of prime numbers.
+
+    Args:
+        x (int): The number of rounds play.
+        nums (list): A list of integers representing the available number.
+
+    Returns:
+        bool: True if the player with the largest number of prime numbers wins, False otherwise.
+    """
+    def isPrime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    total = sum(isPrime(i) for i in range(2, max(nums) + 1))
+    return total % 2 != 0
